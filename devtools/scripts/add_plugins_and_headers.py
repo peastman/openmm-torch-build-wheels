d93d71c7a7c3d6cd9c19ccb561569a2d81c2b793
peastman <peastman@stanford.edu>
2024-11-06 20:21:07 -0800

    Debugging

--- devtools/scripts/add_plugins_and_headers.py
+++ devtools/scripts/add_plugins_and_headers.py
@@ -10,17 +10,20 @@ if len(sys.argv) == 2:
 else:
     platforms = sys.argv[2:]
     def ignore(dir, files):
+        print('  dir:', dir)
+        print('  files:', files)
         ignorefiles = []
         for platform in platforms:
             if platform[0] == '!':
                 ignorefiles += [f for f in files if platform[1:] in f]
             else:
                 ignorefiles += [f for f in files if platform not in f and not isdir(join(dir, f))]
-            print('platform:', platform)
-            print('ignorefiles:', ignorefiles)
+            print('    platform:', platform)
+            print('    ignorefiles:', ignorefiles)
         return ignorefiles
 for filename in os.listdir('.'):
     if filename.endswith('.whl'):
+        print('filename:', filename)
         with wheeltools.InWheel(filename, filename):
             shutil.copytree(join(install_dir, 'lib'), 'OpenMM.libs/lib', dirs_exist_ok=True, ignore=ignore)
             # shutil.copytree(join(install_dir, 'include'), 'OpenMM.libs/include', dirs_exist_ok=True)
